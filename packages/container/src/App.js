5e7ad91167b1b496fbb1c04e3ee832810be4f28c
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-11-04 11:13:33 +0100

    avoid collision in css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co"
+});
 export default () => (
   <BrowserRouter>
-    <Header/>
-    <MarketingApp />
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
   </BrowserRouter>
 );
