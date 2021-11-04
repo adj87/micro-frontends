8b25cd5f885963b1e7c8e58a55e8291294b05369
alberto jauregui dominguez <ajauregui@wtelecom.es>
2021-11-04 09:09:24 +0100

    new try

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -2,7 +2,7 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commongConfig = require("./webpack.common");
 const packageJson = require("../package.json");
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCT_DOMAIN;
 
 const prodConfig = {
   mode: "production",
